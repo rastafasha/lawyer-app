f2b1f0826dc33773edff544d28d07a20f6f991dd
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-01-29 16:30:09 -0400

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { HomeComponent } from './pages/home/home.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { SearchComponent } from './pages/search/search.component';
+import { WalletComponent } from './pages/wallet/wallet.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'favorites', component: FavoritesComponent},
+    {path: 'search', component: SearchComponent},
+    {path: 'wallet', component: WalletComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: '**', redirectTo: '', pathMatch: 'full'},
+];
---
+++ src/app/components/aviso/aviso.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aviso',
+  imports: [],
+  templateUrl: './aviso.component.html',
+  styleUrl: './aviso.component.css'
+})
+export class AvisoComponent {
+
+}
